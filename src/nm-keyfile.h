1b6af8b957883acac492a0031ddd92a0cc67f3e7
Lukas Märdian <lukas.maerdian@canonical.com>
2021-01-14 15:42:15 +0100

    nm-keyfile: cleanup

--- src/nm-keyfile.h
+++ src/nm-keyfile.h
@@ -17,7 +17,6 @@
 
 #pragma once
 
-/* YAML emitter */
 #define YAML_MAPPING_OPEN(event, emitter) \
 { \
     yaml_mapping_start_event_initialize(&event, NULL, (yaml_char_t *)YAML_MAP_TAG, 1, YAML_ANY_MAPPING_STYLE); \
@@ -38,6 +37,39 @@
 { \
     yaml_scalar_event_initialize(&event, NULL, (yaml_char_t *)YAML_STR_TAG, (yaml_char_t *)scalar, strlen(scalar), 1, 1, YAML_DOUBLE_QUOTED_SCALAR_STYLE); \
     if (!yaml_emitter_emit(&emitter, &event)) goto error; \
+}
+/* Adding a simple boolean value below, just to produce a valid "access-points:" mapping */
+#define YAML_NETPLAN_WIFI_AP(event, emitter, ssid, hidden) \
+{ \
+    YAML_SCALAR_PLAIN(event, emitter, "access-points"); \
+    YAML_MAPPING_OPEN(event, emitter); \
+    YAML_SCALAR_QUOTED(event, emitter, ssid); \
+    YAML_MAPPING_OPEN(event, emitter); \
+    YAML_SCALAR_PLAIN(event, emitter, "hidden"); \
+    YAML_SCALAR_PLAIN(event, emitter, hidden); \
+    YAML_MAPPING_CLOSE(event, emitter); \
+    YAML_MAPPING_CLOSE(event, emitter); \
+}
+/* open YAML emitter, document, stream and initial mapping */
+#define YAML_OUT_START(event, emitter, file) \
+{ \
+    yaml_emitter_initialize(&emitter); \
+    yaml_emitter_set_output_file(&emitter, file); \
+    yaml_stream_start_event_initialize(&event, YAML_UTF8_ENCODING); \
+    if (!yaml_emitter_emit(&emitter, &event)) goto error; \
+    yaml_document_start_event_initialize(&event, NULL, NULL, NULL, 1); \
+    if (!yaml_emitter_emit(&emitter, &event)) goto error; \
+    YAML_MAPPING_OPEN(event, emitter); \
+}
+/* close initial YAML mapping, document, stream and emitter */
+#define YAML_OUT_STOP(event, emitter) \
+{ \
+    YAML_MAPPING_CLOSE(event, emitter); \
+    yaml_document_end_event_initialize(&event, 1); \
+    if (!yaml_emitter_emit(&emitter, &event)) goto error; \
+    yaml_stream_end_event_initialize(&event); \
+    if (!yaml_emitter_emit(&emitter, &event)) goto error; \
+    yaml_emitter_delete(&emitter); \
 }
 
 /* Some utils for the NetworkManager YAML backend */
